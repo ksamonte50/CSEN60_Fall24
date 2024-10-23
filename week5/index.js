1279f20d587cdcc9283881632cd3e7cfb5922dee
ksamonte50 <ksamonte50@gmail.com>
2024-10-23 11:34:37 -0700

    learned variables and types/operators

--- week5/index.js
+++ week5/index.js
@@ -1,7 +1,7 @@
 // console.log("hello world");
 // alert('Hello, world!');
-window.document.write("<p>Welcome to CSEN 60!</p>");
-document.append("<p>This should come after the h1</p>");
+// window.document.write("<p>Welcome to CSEN 60!</p>");
+// document.append("<p>This should come after the h1</p>");
 
 /*
   Function to calculate the sum of two numbers.
@@ -11,12 +11,54 @@ document.append("<p>This should come after the h1</p>");
 
 // TODO: fix function
 
-//** @description this function adds 2 numbers
-* @param {*} a 
-* @param {*} b 
-* @returns  
-*/
-
+/**
+ * Adds two numbers.
+ * @param {number} a - The first number.
+ * @param {number} b - The second number.
+ * @return {number} The sum of a and b.
+ */
 function add(a, b) {
-    return a + b;
+  return a + b;
 }
+
+let x = 100;  // let x (declaration) = 100 (initialization)
+
+if (true) {
+    x = 200; // Same variable!
+}
+console.log(x); // Outputs: 200
+
+// null != undefined
+let y = null;
+console.log(y);
+
+// String
+let greeting = "Hello, world!";
+let response = `The time is ${new Date()}`;
+console.log(response);
+
+// Boolean & if/else
+const age = 10;
+if (age > 18) {
+  console.log("You are of age")
+} else {
+  console.log(`You are not of age. Please wait ${18 - age} years`)
+}
+
+// Object
+let person = {
+  name: "John",
+  age: 30,
+  isStudent: false
+};
+console.log(person.age);
+console.log(typeof(person)); // Outputs data type of given variable
+
+// Alert (server vs. browser runtime)
+alert("This is an alert box!");
+
+// Strict equality
+console.log(3 === '3'); // false
+
+// Loose equality
+console.log(3 == '3'); // true
